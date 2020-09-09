8a6487ea8348e9ee71a3a080f3f35766a8daaddb
Ricardo da Rocha <ricardo.rocha@positivo.com.br>
2020-09-09 20:05:32 -0300

    Add Future Builder and Loading

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,60 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 
-const request = "https://api.hgbrasil.com/finance?format=json-cors&key=754481f7";
+const request =
+    "https://api.hgbrasil.com/finance?format=json-cors&key=754481f7";
 
-void main() async{
-
-  http.Response response = await http.get(request);
-  print(jsonDecode(response.body)["results"] ["currencies"] ["USD"]);
-
+void main() async {
+  print(await getData());
 
   runApp(MaterialApp(
-      home: Container(),
-      ));
+    home: Home(),
+  ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return jsonDecode(response.body)["results"]["currencies"]["USD"];
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black54,
+        appBar: AppBar(
+          title: Text("\$ Conversor \$"),
+          backgroundColor: Colors.amber,
+          centerTitle: true,
+        ),
+        body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                    child: Text("Carregando Dados...",
+                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                        textAlign: TextAlign.center));
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                      child: Text("Erro ao carregar Dados...",
+                          style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                          textAlign: TextAlign.center));
+                } else {
+                  return Container(color: Colors.green);
+                }
+            }
+          },
+        ));
+  }
 }
